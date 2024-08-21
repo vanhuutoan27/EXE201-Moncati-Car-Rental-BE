caf59cf05c4e709945e9d20b2f1060d5deea5716
QuocDai <145921869+dainq94@users.noreply.github.com>
2024-08-21 20:04:12 +0700

    chang username

--- MocatiCar.Core/Models/auth/MeResponse.cs
+++ MocatiCar.Core/Models/auth/MeResponse.cs
@@ -3,7 +3,7 @@
     public class MeResponse
     {
         public Guid Id { get; set; }
-        public string UserName { get; set; }
+        public string Username { get; set; }
         public string Email { get; set; }
         public string Avatar { get; set; }
         public string Role { get; set; }
--- MocatiCar.Core/Models/content/Requests/UpdateUserRequest.cs
+++ MocatiCar.Core/Models/content/Requests/UpdateUserRequest.cs
@@ -7,7 +7,7 @@
         public string? Avatar { get; set; }
         public string? Gender { get; set; }
 
-        public string? UserName { get; set; }
+        public string? Username { get; set; }
         public string? Email { get; set; }
         public string? PhoneNumber { get; set; }
         public string Role { get; set; }
