7384c255d9b0e9c30c2e9cfb36d3662af5d815d1
DuyPham12345 <saophadao1472@gmail.com>
2024-09-18 17:04:31 +0700

    more code

---
+++ MocatiCar.Core/Models/content/Responses/LocationDetailRespone.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MocatiCar.Core.Models.content.Responses
+{
+    public class LocationDetailRespone
+    {
+         public string location { get; set; }
+        public string locationDetails {get ; set;}
+       
+    }
+}
